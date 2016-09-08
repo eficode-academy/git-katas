444c5e65457cd77bc1f06a0d1a1931d7204a4370
Johan Abildskov <randomsort@gmail.com>
2016-09-08 20:57:12 +0200

    kata3 setupsh

--- setup.sh
+++ setup.sh
@@ -1,24 +1,7 @@
-kata="kata1-merge-conflict"
+kata="kata3-detached-head"
 
-git checkout -b $kata-branch1
+git checkout HEAD~3
 
-echo "This is a relevant fact" > file.txt
-
-git add file.txt
-
-git commit -m "add relevant fact"
-
-git checkout master
-
-git checkout -b $kata-branch2
-
-echo "This is an indispensable truth!" > file.txt
-
-git add file.txt
-
-git commit -m "add indispensable truth!"
-
-git checkout $kata
 
 
 
