754b21dfa3115f77f7413a7621e445b5b652c653
Johan Abildskov <randomsort@gmail.com>
2016-09-08 20:23:37 +0200

    tinker with setup.sh

--- setup.sh
+++ setup.sh
@@ -4,13 +4,17 @@ git checkout -b $kata-branch1
 
 echo "This is a relevant fact" > file.txt
 
-git commit -am "add relevant fact"
+git add file.txt
+
+git commit -m "add relevant fact"
 
 git checkout -b $kata-branch2
 
 echo "This is an indispensable truth!" > file.txt
 
-git commit -am "add indispensable truth!"
+git add filt.txt
+
+git commit -m "add indispensable truth!"
 
 
 
