cd189383dca8c4152b10c3f5b321f953dad70387
Johan Abildskov <randomsort@gmail.com>
2016-11-08 20:05:39 +0100

    split git commit

--- setup.sh
+++ setup.sh
@@ -2,5 +2,6 @@ kata="basic-branching"
 git checkout -b $kata-master
 git checkout -b $kata-second-branch
 echo "test" > file1.txt
-git commit -am "Add file1.txt"
+git add file1.txt
+git commit -m "Add file1.txt"
 git checkout $kata-master
