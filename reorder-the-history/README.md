e11372e033b04017bc19c5765f971b83f8f1e546
Johan Abildskov <randomsort@gmail.com>
2016-11-29 17:11:06 +0100

    add reorder-the-history

---
+++ reorder-the-history/README.md
@@ -0,0 +1,5 @@
+# gitkatas
+# Kata 7: Reordering history
+The commits here has obviously been made by a mad man.
+Unfortunately they actually contain useful information - it's just that the history is weird.
+You should fix this such that our `git log` looks great!
---
+++ reorder-the-history/setup.sh
@@ -0,0 +1,40 @@
+kata="kata7-reorder-the-history"
+
+git checkout -b $kata-master
+
+echo "1" > file1
+git add file1
+git commit -m "file1"
+
+echo "9" > file9
+git add file9
+git commit -m "file9"
+
+echo "8" > file8
+git add file8
+git commit -m "file8"
+
+echo "3" > file3
+git add file3
+git commit -m "file3"
+
+echo "4" > file4
+git add file4
+git commit -m "file4"
+
+echo "5" > file5
+git add file5
+git commit -m "file5"
+
+echo "2" > file2
+git add file2
+git commit -m "file2"
+
+echo "6" > file6
+git add file6
+git commit -m "file6"
+
+echo "7" > file7
+git add file7
+git commit -m "file7"
+
