940382f147149c38cee625359c0f4c94f5b7b5c3
Johan Abildskov <randomsort@gmail.com>
2016-11-29 17:09:07 +0100

    save my commit

---
+++ save-my-commit/README.md
@@ -0,0 +1,7 @@
+# gitkatas
+## Kata6: Save my commit
+We've lost the holy grail!
+Save it!
+
+## Relevant git commands
+* `git reflog`
---
+++ save-my-commit/setup.sh
@@ -0,0 +1,23 @@
+kata="kata6-save-my-commit"
+
+git checkout -b $kata-master
+
+echo "This is a relevant fact" > file.txt
+
+git add file.txt
+
+git commit -m "add relevant fact"
+
+echo "This is an indispensable truth!" > file.txt
+
+git add file.txt
+
+git commit -m "add indispensable truth!"
+
+echo "42" > holygrail.txt
+
+git add holygrail.txt
+
+git commit -m "found the holy grail"
+
+git reset --hard HEAD~3
