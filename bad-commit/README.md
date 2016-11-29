e252ef8e12b23eb503cdf4d1f553b46406f712f9
Johan Abildskov <randomsort@gmail.com>
2016-11-29 17:04:02 +0100

    add bad commit

---
+++ bad-commit/README.md
@@ -0,0 +1,4 @@
+# gitkatas
+## kata4: Bad Commit
+One of the commits on `kata4-bad-commit-master` introduced a bad file.
+Find the commit and revert it.
---
+++ bad-commit/setup.sh
@@ -0,0 +1,24 @@
+kata="kata4-bad-commit"
+
+git checkout -b $kata-master
+
+touch file1
+git add file1
+git commit -m "File1"
+
+touch file2
+git add file2
+git commit -m "File2"
+
+touch badfile
+git add badfile
+git commit -m "File3"
+
+touch file4
+git add file4
+git commit -m "File4"
+
+touch file5
+git add file5
+git commit -m "File5"
+
