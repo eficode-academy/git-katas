61bb9aeb886a42675ab107b97b82217df3ba5127
Mike Long <mike@praqma.com>
2016-12-18 20:42:28 +0100

    Update the confilct exercise

--- merge-conflict/README.md
+++ merge-conflict/README.md
@@ -1,8 +1,8 @@
 # gitkatas
 ## Kata 1 merge conflict
-In this kata git can't figure how to merge the content added on `kata1-merge-conflict-branch1` with the content on `kata1-merge-conflict-branch2`.
+In this kata git can't figure how to merge the content added on `kata1-merge-conflict-branch1` with the content on `master`.
 
-Both changes needs to be in branch1 when you're done.
+Both changes needs to be in master when you're done.
 
 ## Relevant commands
 - `git merge`
--- merge-conflict/setup.sh
+++ merge-conflict/setup.sh
@@ -1,23 +1,22 @@
-kata="kata1-merge-conflict"
+kata="ff-merge"
+mkdir exercise
+cd exercise
+git init
+touch greeting.txt
+git add greeting.txt
+git commit -m "Add file greeting.txt"
+echo "hello" > greeting.txt
+git add greeting.txt
+git commit -m "Add content to greeting.txt"
 
+# Create a file branch1
 git checkout -b $kata-branch1
-
 echo "This is a relevant fact" > file.txt
-
 git add file.txt
-
 git commit -m "add relevant fact"
 
 
-git checkout $kata
-
-git checkout -b $kata-branch2
-
+git checkout master
 echo "This is an indispensable truth!" > file.txt
-
 git add file.txt
-
 git commit -m "add indispensable truth!"
-
-git checkout $kata-branch1
-
