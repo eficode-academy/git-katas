c50886237a31539976f7fd6fc6c49fc63053b9af
Johan Abildskov <randomsort@gmail.com>
2017-01-02 19:44:11 +0100

    fix issue with branching from empty repo

--- basic-branching/setup.sh
+++ basic-branching/setup.sh
@@ -2,8 +2,12 @@ kata="basic-branching"
 mkdir exercise
 cd exercise
 git init
+git checkout -b $kata-master
+echo "dummy" > dummy.txt
+git add dummy.txt
+git commit -m "dummy commit"
 git checkout -b $kata-second-branch
 echo "test" > file1.txt
 git add file1.txt
 git commit -m "Add file1.txt"
-git checkout -b $kata-master
+git checkout $kata-master
