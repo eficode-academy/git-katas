2a36f7eb44d707bb62cd7c0cdab6d15716d6c1dc
Martin Mosegaard Amdisen <martin.amdisen@praqma.com>
2017-01-29 12:22:23 +0100

    Update setup comment

--- merge-conflict/setup.sh
+++ merge-conflict/setup.sh
@@ -9,7 +9,7 @@ echo "hello" > greeting.txt
 git add greeting.txt
 git commit -m "Add content to greeting.txt"
 
-# Create a file branch1
+# Create a file on branch1
 git checkout -b $kata-branch1
 echo "This is a relevant fact" > file.txt
 git add file.txt
