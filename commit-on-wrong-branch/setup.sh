65987911bb8e6025a61628173023e4bda6182616
Johan Abildskov <randomsort@gmail.com>
2017-03-11 12:37:10 +0100

    fix setup.sh

--- commit-on-wrong-branch/setup.sh
+++ commit-on-wrong-branch/setup.sh
@@ -1,6 +1,17 @@
 kata="kata5-commit-on-wrong-branch"
+rm -rf exercise
+
+mkdir exercise 
+cd exercise
+
+git init
 
 git checkout -b $kata-master
+
+touch file.txt
+
+git add file.txt
+git commit -m "Initial commit"
 
 echo "This is a relevant fact" > file.txt
 
@@ -15,3 +26,4 @@ echo "This is a very quick fix" > fix.txt
 git add fix.txt
 
 git commit -m "Make quick fix - should be moved to master"
+
