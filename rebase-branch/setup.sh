d403542c2d74dae2b7ff804da5974950b0e78ce1
Johan Abildskov <randomsort@gmail.com>
2017-05-13 18:17:21 +0200

    close #83 align rebasebranch

--- rebase-branch/setup.sh
+++ rebase-branch/setup.sh
@@ -1,10 +1,12 @@
+#/bin/bash
+
+# Include utils
+source ../utils/utils.sh
 kata="rebase-branch"
 set -e
 
-rm -rf exercise
-mkdir exercise
-cd exercise
-git init
+makerepo
+
 touch greeting.txt
 git add greeting.txt
 git commit -m "Add file greeting.txt"
