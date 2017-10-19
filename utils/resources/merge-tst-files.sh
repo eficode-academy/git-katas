6836787ea3a931f975e7a859bb18a036cb893791
Thierry Lacour <thi@praqma.net>
2017-10-19 12:55:34 +0200

    Add merge-driver kata

---
+++ utils/resources/merge-tst-files.sh
@@ -0,0 +1,19 @@
+echo ""
+echo "#########################"
+echo "# TST merge tool called #"
+echo "#########################"
+echo ""
+echo "Resolving conflicts..."
+echo '[                    ]\r\c'
+sleep 1
+echo '[===                 ]\r\c'
+sleep 1
+echo '[===========         ]\r\c'
+sleep 1
+echo '[====================]\r\c'
+sleep 1
+echo "[H A R M O N Y]" > $2
+echo '\nConflict resolved!'
+sleep 1
+echo ""
+exit 0
