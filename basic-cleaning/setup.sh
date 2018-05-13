ed1844d70eb19465c5bfe44171a0f9cdb1b0001d
tom hostyn <tom.hostyn@gmail.com>
2018-05-13 11:55:43 +0200

    mv source to src. add README.txt

--- basic-cleaning/setup.sh
+++ basic-cleaning/setup.sh
@@ -6,16 +6,19 @@ source ../utils/utils.sh
 kata="basic-cleaning"
 
 makerepo 
+mkdir src
 
-echo "// awsome C code " > myapp.c
-echo "// awsome C header " > myapp.h
+echo "** SOME USEFUL INFO ** " > README.txt
+echo "// awsome C code " > src/myapp.c
+echo "// awsome C header " > src/myapp.h
 
-git add myapp.*
+git add src/myapp.* README.txt
 git commit -m "C code for my app"
 
-echo "// awsome C library  " > mylib.c
+echo "// awsome C library  " > src/mylib.c
 
-touch myapp.c~
+touch src/myapp.c~ src/oldfile.c~ 
+touch README.txt~
 mkdir obj
-touch mylib.o mylib.a myapp.o a.out
+touch obj/mylib.o obj/mylib.a obj/myapp.o obj/a.out
 
