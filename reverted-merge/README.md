3038ad952834b0e3b0a7e82115b9684cc3b52e7e
Andre Kappes <andre.kappes@andrena.de>
2018-07-20 18:54:53 +0200

    Remove revert and put in as task

--- reverted-merge/README.md
+++ reverted-merge/README.md
@@ -11,9 +11,15 @@ prudent, you do this on a branch `integrate-library-1.2.4`.
 
 Unfortunately, you discover after your merge that the library has a bug, which
 has to be fixed by this other team. To prevent from releasing it into production,
-you revert the merge commit.
+you decide to revert the merge commit.
 
 ## The task
-* Take the role of the library team and fix the bug on the branch
+* Revert the merge commit
+* Take the role of the library team and fix the bug in the library on the branch
 * Explore how you can get the changes from the branch into the master again  
   Try to merge first to see what happens
+
+## Useful commands
+* `git revert`
+* `git rebase`
+* `git merge`
--- reverted-merge/setup.sh
+++ reverted-merge/setup.sh
@@ -21,38 +21,41 @@ git branch integrate-library-1.2.4
 git checkout integrate-library-1.2.4
 
 echo "library-1.2.4" > lib.txt
+echo "New libarry funtcionality" >> lib.txt
 echo "module using library-1.2.4" > mymodule.txt
 git add lib.txt
 git add mymodule.txt
 git commit -m"Update to library version 1.2.4"
 
-echo "New libarry funtcionality" >> lib.txt
-git add lib.txt
-git commit -m"Add new library functionality"
 echo "Use new library functionality" >> mymodule.txt
 git add mymodule.txt
 git commit -m"Use new functionality in mymodule"
 
+
 git checkout master
-echo "Add promising feature X" >> mymodule.txt
+echo "Promising feature X" >> mymodule.txt
 git add mymodule.txt
 git commit -m"Add feature X"
+
 
 git merge integrate-library-1.2.4 --no-edit
 # deal with merge conflict
 echo "module using library-1.2.4" > mymodule.txt
-echo "Add promising feature X" >> mymodule.txt
+echo "Promising feature X" >> mymodule.txt
 echo "Use new library functionality" >> mymodule.txt
 git add mymodule.txt
 git commit -m"Merge integrate-library-1.2.4" --no-edit
 
-echo "Add useful feature Y" >> mymodule.txt
+
+echo "Useful feature Y" >> mymodule.txt
 git add mymodule.txt
 git commit -m"Add feature Y"
 
-git revert HEAD~1 -m 1 --no-edit
-echo "module using library-1.2.3" > mymodule.txt
-echo "Add promising feature X" >> mymodule.txt
-echo "Add useful feature Y" >> mymodule.txt
-git add mymodule.txt
-git commit --no-edit
+
+# git revert HEAD~1 -m 1 --no-edit
+# # deal with revert conflict
+# echo "module using library-1.2.3" > mymodule.txt
+# echo "Promising feature X" >> mymodule.txt
+# echo "Useful feature Y" >> mymodule.txt
+# git add mymodule.txt
+# git commit --no-edit
