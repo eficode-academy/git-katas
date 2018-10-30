905aae7b63b2a6b5708f23160fa467f2d0623fd8
Samuel Ytterbrink <say@praqma.net>
2018-10-30 13:14:49 +0100

    First version of rebase-exec.

---
+++ rebase-exec/README.md
@@ -0,0 +1,9 @@
+#Rebase Exec
+
+Verify commits before push
+
+run the tests on every commit since the initial commit
+
+`git rebase -i --exec test.sh initial-commit`
+
+Amend the broken commit and continue rebasing.
---
+++ rebase-exec/setup.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+source ../utils/utils.sh
+
+makerepo
+
+echo "#! /usr/bin/env sh" > 'test.sh'
+echo "echo 'all tests pass'" >> 'test.sh'
+echo "exit 0" >> 'test.sh'
+chmod +x 'test.sh'
+git add 'test.sh'
+git commit -m "Initial commit"
+git tag initial-commit
+
+touch 1.txt
+git add 1.txt
+git commit -m "1"
+
+touch 2.txt
+git add 2.txt
+git commit -m "2"
+
+touch 3.txt
+git add 3.txt
+echo "#! /usr/bin/env sh" > 'test.sh'
+echo "echo 'One failing test'" >> 'test.sh'
+echo "exit 1" >> 'test.sh'
+git add 'test.sh'
+git commit -m "3"
+
+touch 4.txt
+git add 4.txt
+git commit -m "4"
+
+touch 5.txt
+git add 5.txt
+echo "#! /usr/bin/env sh" > 'test.sh'
+echo "echo 'all tests pass'" >> 'test.sh'
+echo "exit 0" >> 'test.sh'
+git add 'test.sh'
+git commit -m "5"
+
+touch 6.txt
+git add 6.txt
+git commit -m "6"
+
+
