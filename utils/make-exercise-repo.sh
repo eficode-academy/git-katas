b4bc03ceaf44a247c0a06ee2624bdb6fcea33d12
Jan Krag <jankrag@gmail.com>
2019-08-27 09:44:31 +0200

    Add simple way of setting up fake remote for exercises

--- utils/make-exercise-repo.sh
+++ utils/make-exercise-repo.sh
@@ -11,3 +11,16 @@ makerepo() {
     cd exercise
 
 }
+
+makefakeremoterepo() {
+
+    # First cleanup if there is an old exercise repository
+    rm -rf remote/
+
+    # Initialize a new repository
+    git init remote
+
+    # Go there
+    cd remote
+
+}
