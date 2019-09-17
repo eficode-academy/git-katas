97ea671f4a9ecb10b852425e4b34584aae6832a1
Mads Jensen <maj@praqma.net>
2019-09-17 12:12:30 +0200

    Added kata about git tags.

---
+++ git-tag/setup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Include utils
+source ../utils/utils.sh
+
+kata="$(basename $(pwd))"
+makerepo
+
+echo "dummy" > dummy.txt
+git add dummy.txt
+git commit -m "dummy commit"
+git tag "dummy"
+echo "dummy2" > dummy.txt
+git commit -m "adding more content to dummy.txt"
+git tag -a "dummy2" -m "don't worry, be happy"
