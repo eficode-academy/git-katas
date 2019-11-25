d9711cd512a99733866467c16a49e1229b65699f
Michael Ingeman-Nielsen <min@praqma.net>
2019-11-25 01:03:48 +0100

    Expand  on  submodules kata

--- submodules/setup.ps1
+++ submodules/setup.ps1
@@ -1,21 +1,36 @@
-. ..\utils\make-exercise-repo.ps1
+# First cleanup if there is an old exercise repository
+if (Test-Path .\exercise) {
+	Remove-Item .\exercise\ -force -recurse
+}
 
-New-Item component -ItemType Directory | Out-Null
-Set-Location component
+New-Item -ItemType Directory -Path exercise | Out-Null
+Set-Location .\exercise\
+
+# Create component repo
+New-Item -ItemType Directory -Path .\remote\component | Out-Null
+Set-Location .\remote\component
+git init
+
 Set-Content -Value "" -Path component.h
-
-git init
 git add component.h
-git commit -m "Touch component header."
+git commit -m "Touch component header"
 
-Set-Location .\..
+# Convert to a bare repository and delete the original working directory.
+Move-Item -Path ".git" -Destination ..\component-repo.git
+Set-Location ..\component-repo.git
+git config --bool core.bare true
+Set-Location ..
+Remove-Item -Path ./component -Force -Recurse
+Set-Location ..
 
-New-Item product -ItemType Directory | Out-Null
-Set-Location product
-Set-Content -Value "" -Path product.h
+# And clone it so that it is ready for the exrcise.
+git clone remote/component-repo.git component
 
-git init
-git add product.h
-git commit -m "Touch product header."
+# Create a product repository.
+git init product
+Set-Location -Path .\product
+Set-Content -Value "" -Path .\product.h
+git add .\product.h
+git commit -m "Touch product header"
 
 Set-Location .\..
--- submodules/setup.sh
+++ submodules/setup.sh
@@ -13,16 +13,27 @@ mkdir ${EXERCISE_DIR}
 
 cd ${EXERCISE_DIR}
 
-mkdir component
+# Create component repo
+mkdir -p remote/component
+git init remote/component
+cd remote/component
 
-cd component
-git init
 touch component.h
 git add component.h
-git commit -m "Touch component header."
+git commit -m "Touch component header"
 
+# Convert to a bare repository and delete the original working directory.
+mv .git ../component-repo.git
+cd ../component-repo.git
+git config --bool core.bare true
+cd ..
+rm -rf component/
 cd ..
 
+# And clone it so that it is ready for the exrcise.
+git clone remote/component-repo.git component
+
+# Create a product repository.
 mkdir product
 cd product
 git init
