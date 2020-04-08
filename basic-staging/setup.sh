8c48cc709ef8f01ea676588ab98708fdc5f31663
Sudheer Kumar Chiluveri <Sudheer.Chiluveri@gmail.com>
2020-04-08 14:53:26 +0300

    helper functions to set and unset local user for training bot commits. Updated setup.sh instructions in few katas to take new effect in place.

--- basic-staging/setup.sh
+++ basic-staging/setup.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
+# kata: basic-staging
+
 # Include utils
 source ../utils/utils.sh
-makerepo
 
-kata="basic-staging"
+pre-setup # Make exercise repo, and setup necessary.
+
 echo 1 > file.txt
 git add file.txt
 git commit -m "1"
+
+post-setup
