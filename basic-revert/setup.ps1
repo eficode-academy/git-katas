afecd6355eef8ed16f57dbc019604cf7d93580d1
Johan Abildskov <randomsort@gmail.com>
2020-04-29 12:32:49 +0200

    Simplify basic revert
    close #177

--- basic-revert/setup.ps1
+++ basic-revert/setup.ps1
@@ -5,12 +5,16 @@ Set-Content -Value "" -Path greeting.txt
 git add greeting.txt
 git commit -m "Add file greeting.txt"
 
-Set-Content -Value "we want to revert back to this" -Path greeting.txt
+Set-Content -Value "supersecretpassword" -Path credentials.txt
+git add credentials.txt
+git commit -m "Add credentials to repository"
+
+Set-Content -Value "Original File Content" -Path greeting.txt
 
 git add greeting.txt
 git commit -m "Add content to greeting.txt"
 
-Set-Content -Value "this should have been appended to the original content" -Path greeting.txt
+Set-Content -Value "This should have been appended to the original content, rather than overwriting it" -Path greeting.txt
 
 git add greeting.txt
-git commit -m "Add more content to greeting.txt"
+git commit -m "Overwrite greeting.txt"
--- basic-revert/setup.sh
+++ basic-revert/setup.sh
@@ -10,11 +10,14 @@ pre-setup # Make exercise repo, and setup necessary.
 touch greeting.txt
 git add greeting.txt
 git commit -m "Add file greeting.txt"
-echo "we want to revert back to this" > greeting.txt
+echo "supersecretpassword" > credentials.txt
+git add credentials.txt
+git commit -m "Add credentials to repository"
+echo "Original file content" > greeting.txt
 git add greeting.txt
 git commit -m "Add content to greeting.txt"
-echo "this should have been appended to the original content" > greeting.txt
+echo "This should have been appended to the original content, rather than overwriting it." > greeting.txt
 git add greeting.txt
-git commit -m "Add more content to greeting.txt"
+git commit -m "Overwrite greeting.txt"
 
 post-setup
