98cd88b6d499403a30bbf1a1fb2cb3fb6ee1256b
Frank Theile <ftheile@grundfos.com>
2020-05-14 10:24:48 +0200

    Add fake author in master-based-workflow exercise
    To easily distinguish own commits from the "virtual" collaborator's
    ones.

--- master-based-workflow/fitzgerald-pushes-before-we-do.sh
+++ master-based-workflow/fitzgerald-pushes-before-we-do.sh
@@ -4,5 +4,5 @@ cd ../fake-remote-repository
 git checkout master
 touch fitz-was-here.md
 git add fitz-was-here.md
-git commit -m "Fitz made this"
+git commit -m "Fitz made this" --autor "R. Fitzgerald <rfitz@example.com>"
 git checkout HEAD~0
