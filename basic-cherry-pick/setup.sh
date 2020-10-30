3004959c79e0f83e39833c9869daed7e77789d86
Johan Abildskov <randomsort@gmail.com>
2020-10-30 10:16:21 +0100

    [Cherry-pick] Fixed some wordings

--- basic-cherry-pick/setup.sh
+++ basic-cherry-pick/setup.sh
@@ -10,13 +10,13 @@ pre-setup # Make exercise repo, and setup necessary.
 #Commit a
 echo -e "Ben\nTom\nSally" > names.txt
 git add names.txt
-git commit -m "Added the names file"
+git commit -m "Commit A: Added the names file"
 
 #Common base commit
 #Commit b
 echo "This is a lovely sentence" > sentence.txt
 git add sentence.txt
-git commit -m "Added the sentence file"
+git commit -m "Commit B: Added the sentence file"
 
 #Branch away to feature
 git branch feature
@@ -24,12 +24,12 @@ git branch feature
 #Commit c
 echo "This is another delicous sentence" >> other_sentence.txt
 git add other_sentence.txt
-git commit -m "Added the additional other_sentence file"
+git commit -m "Commit C: Added the additional other_sentence file"
 
 #Commit d
 echo -e "Cat\nDog\nMouse" >> animals.txt
 git add animals.txt
-git commit -m "Added the animals file"
+git commit -m "Commit D: Added the animals file"
 
 #Switch now to feature (commit b)
 git checkout feature
@@ -37,7 +37,7 @@ git checkout feature
 #Commit e
 echo "1\n2\n6\n54" >> numbers.txt
 git add numbers.txt
-git commit -m "Added the numbers file"
+git commit -m "Commit E: Added the numbers file"
 
 #Commit f
 echo -e "Craig\nJodie\nNathan" >> names.txt
@@ -52,7 +52,7 @@ git commit -m "Commit G: Updated the original sentence file"
 #Commit h
 echo "Boring extra file for no reason" > boring.txt
 git add boring.txt
-git commit -m "Added the boring file"
+git commit -m "Commit H: Added the boring file"
 
 git checkout master
 
