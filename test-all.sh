f6a64f82fec1d0f26289714d7fe9fceccc2a2f8e
Jan Krag <jankrag@gmail.com>
2021-03-12 12:26:38 +0100

    Run all tests in lexicographical order
    Better readability, when test output is listed in same order
    as in file system and on GitHub :-)

--- test-all.sh
+++ test-all.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 echo "Testing all kata setup scripts"
-find . -name "setup.sh" -type f -execdir ../utils/test/test_setup.sh  \;
+find -s . -name "setup.sh" -type f -execdir ../utils/test/test_setup.sh  \;
