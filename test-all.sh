ea1a3359f53afb95aab02afa0783c7fe6a28882b
Mads Jensen <maj@praqma.net>
2021-03-15 14:21:40 +0100

    Use a function

--- test-all.sh
+++ test-all.sh
@@ -1,4 +1,22 @@
 #!/usr/bin/env bash
 
+basedir=$(git rev-parse --show-toplevel)
+
 echo "Testing all kata setup scripts"
-find -s . -name "setup.sh" -type f -execdir ../utils/test/test_setup.sh  \;
+
+function test-setup (){
+  return_code=0
+  scripts=$(find . -name "setup.sh" -type f -print | sort)
+  for script in ${scripts}; do
+    cd $(dirname ${script})
+    ../utils/test/test_setup.sh setup.sh
+    exit_code=$?
+    if [ ${exit_code} -ne 0 ]; then
+      return_code=${exit_code}
+    fi
+    cd $basedir
+  done
+  return $return_code
+}
+
+test-setup
