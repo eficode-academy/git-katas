51ab543a31713dc5f19fa1c78a312a3b6bcb7f13
Jan Krag <jankrag@gmail.com>
2021-03-26 13:29:28 +0100

    Test on both bash and zsh
    - Allow control of which shell is used in test-all script
    - Add job to CI Action that tests on Zsh on mac
    - Cleanup GH actions naming & remove redundant Docker based zsh job
    
    Co-author: @zanderhavgaard

--- test-all.sh
+++ test-all.sh
@@ -1,9 +1,27 @@
 #!/usr/bin/env bash
+# shellcheck disable=SC1091
+
 GREEN='\033[0;32m'
 RED='\033[0;31m'
 RC='\033[0m' # Reset Color
 
 basedir=$(git rev-parse --show-toplevel)
+
+usage() {
+  echo "Run $0 to test all kata setup.sh scripts"
+  echo "Override shell to test in with e.g. $0 -s zsh"
+  exit 0
+}
+
+RUNSHELL=bash
+while getopts s:h option; do
+  case "${option}"
+    in
+      s) RUNSHELL=${OPTARG};;
+      h) usage;;
+      *) usage;;
+  esac
+done
 
 echo "Testing all kata setup scripts"
 
@@ -15,7 +33,7 @@ function test-setup (){
   for script in ${scripts}; do
     ((total++))
     cd "$(dirname "${script}")" || exit
-    ../utils/test/test_setup.sh setup.sh
+    $RUNSHELL  ../utils/test/test_setup.sh setup.sh 2>&1
     exit_code=$?
     if [ ${exit_code} -ne 0 ]; then
       return_code=${exit_code}
