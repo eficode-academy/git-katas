f65f4cf7d33aad464620e1eea2abf13d88667e95
Jan Krag <jan.krag@eficode.com>
2022-08-19 01:08:00 +0200

    Maybe a more cross-platform way of creating large dummy file
    truncate apparently wasn't very cross-platform.
    Also having all the files basically "empty" and alike could cause confusion.
    Now I create files with actual dummy text content that are at least
    1 Mb in size using a while loop.

--- lfs/setup.sh
+++ lfs/setup.sh
@@ -14,15 +14,29 @@ echo "Stuff" > stuff.txt
 git add . 
 git commit -m 'Some small files'
 
-truncate -s 1M file1.mp3
+# Create a dummy mp3 file in a way that is more cross-platform than `truncate -s 1M file1.mp3``
+echo "Dummy sound data" > tmpfile1;
+while [ "$(wc -c < tmpfile1)" -le 1048576 ]; do 
+    cat tmpfile1 >> tmpfile2
+    cat tmpfile2 >> tmpfile1
+done
+rm tmpfile2
+mv tmpfile1 file1.mp3
 
 git add file1.mp3
 git commit -m 'Add actual mp3'
 
 git push
 
-truncate -s 1M file2.mpeg
-
+#truncate -s 1M file2.mpeg
+# Create a dummy mpeg file in a way that is more cross-platform than `truncate -s 1M <file name>`
+echo "Very fake and repetitive video data" > tmpfile1;
+while [ "$(wc -c < tmpfile1)" -le 1048576 ]; do 
+    cat tmpfile1 >> tmpfile2
+    cat tmpfile2 >> tmpfile1
+done
+rm tmpfile2
+mv tmpfile1 file2.mpeg
 
 
 post-setup
