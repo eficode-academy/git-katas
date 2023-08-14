6c8eef81fde37aa5329f3e0711ecd9682bf2b104
Jan Krag <jan.krag@eficode.com>
2023-08-14 22:44:26 +0300

    Add sleep statments to setup
    This whould help give commits unique timestamps
    
    to fix weird sorting observerd in git log

--- restore/setup.sh
+++ restore/setup.sh
@@ -9,13 +9,15 @@ echo "the original foo" > foo.txt
 git add foo.txt
 git commit -m "Add foo.txt"
 git tag v1.0.0
+sleep 1
 echo "bar" > bar.txt
 git add bar.txt
 git commit -m "Add bar.txt"
+sleep 1
 echo "foo" > foo.txt
 git add foo.txt
 git commit -m "Update foo.txt"
-
+sleep 1
 echo "FIXME: DONT COMMIT" > bar.txt
 git add bar.txt
 rm foo.txt
